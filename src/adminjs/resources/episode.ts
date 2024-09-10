37f56fc1b5d57d90a05e6c49aa1db3c77a579578
Pedro Machado <pedroeugenio.dev@gmail.com>
2024-09-10 12:09:38 -0300

    Adiciona uma feature para upload de arquivos para o formulário de criação de episodios

--- src/adminjs/resources/episode.ts
+++ src/adminjs/resources/episode.ts
@@ -1,4 +1,6 @@
-import { ResourceOptions } from "adminjs";
+import path from "path";
+import uploadFileFeature from "@adminjs/upload";
+import { FeatureType, ResourceOptions } from "adminjs";
 
 export const episodeResourceOptions: ResourceOptions = {
   navigation: "Catálogo",
@@ -31,3 +33,20 @@ export const episodeResourceOptions: ResourceOptions = {
     "updatedAt",
   ],
 };
+
+export const episodeResourceFeatures: FeatureType[] = [
+  uploadFileFeature({
+    provider: {
+      local: {
+        bucket: path.join(__dirname, "../../../uploads"),
+        opts: { baseUrl: "/uploads" },
+      },
+    },
+    properties: {
+      key: "videoUrl",
+      file: "uploadVideo",
+    },
+    uploadPath: (record, filename) =>
+      `videos/course-${record.get("courseId")}/${filename}`,
+  }),
+];
