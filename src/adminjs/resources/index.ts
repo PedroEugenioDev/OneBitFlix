14490e0e5ec3b0267ccd99d4079d0e8802ddc631
Pedro Machado <pedroeugenio.dev@gmail.com>
2024-09-10 12:10:34 -0300

    Adiciona a feature de upload ao resouce de episode

--- src/adminjs/resources/index.ts
+++ src/adminjs/resources/index.ts
@@ -2,12 +2,13 @@ import { ResourceWithOptions } from "adminjs";
 import { Category, Course, Episode } from "../../models";
 import { categoryResourceOptions } from "./category";
 import { courseResourceOptions } from "./course";
-import { episodeResourceOptions } from "./episode";
+import { episodeResourceFeatures, episodeResourceOptions } from "./episode";
 
 export const adminJsResources: ResourceWithOptions[] = [
   {
     resource: Episode,
     options: episodeResourceOptions,
+    features: episodeResourceFeatures,
   },
   {
     resource: Course,
