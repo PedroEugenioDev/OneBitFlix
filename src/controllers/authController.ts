268fd1980fa60dbcd0c9e5c3f8e7a69667eceb5f
Pedro Machado <pedroeugenio.dev@gmail.com>
2024-09-18 16:29:31 -0300

    Criando rota de Login com autenticação JWT

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express'
 import { userService } from '../services/userServices'
+import { jwtService } from '../services/jwtServices'
 
 export const authController = {
   register: async (req: Request, res: Response) => {
@@ -24,5 +25,34 @@ export const authController = {
         return res.status(400).json({ message: err.message })
       }
     }
-  }
+  },
+
+  login: async (req: Request, res: Response) => {
+    const { email, password } = req.body
+    try {
+      const user = await userService.findByEmail(email)
+      if (!user) {
+        return res.status(401).json({ message: 'E-mail não registrado' })
+      }
+      user.checkPassword(password, (err, isSame) => {
+        if (err) {
+          return res.status(400).json({ message: err.message })
+        }
+        if (!isSame) {
+          return res.status(401).json({ message: 'Senha incorreta' })
+        }
+				const payload = {
+          id: user.id,
+          firstName: user.firstName,
+          email: user.email
+        }
+        const token = jwtService.signToken(payload, '7d')
+        return res.json({ authenticated: true, user, token })
+      })
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message })
+      }
+    }
+  },
 }
