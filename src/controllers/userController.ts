5d2b0f69ed5a75e9b37b3361464f66303f7d09ae
Pedro Machado <pedroeugenio.dev@gmail.com>
2024-09-20 19:12:39 -0300

    Adicionando rota GET para dados do usuário logado

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -3,6 +3,17 @@ import { AuthenticatedRequest } from "../middleware/auth";
 import { userService } from "../services/userServices";
 
 export const usersController = {
+    show: async (req: AuthenticatedRequest, res: Response) => {
+        try {
+            const currentUser = req.user!
+            return res.json(currentUser)
+        } catch (err) {
+            if (err instanceof Error) {
+              return res.status(400).json({ message: err.message })
+            }
+        }
+    },
+
   watching: async (req: AuthenticatedRequest, res: Response) => {
     const { id } = req.user!
     try {
